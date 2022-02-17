a614d444c8ecc6dcc322c21e65b149d976140fd8
F-mm1229 <meru112mofmof129@gmail.com>
2022-02-17 17:03:15 +0900

    6:05

--- script.js
+++ script.js
@@ -3,4 +3,39 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 720;
+
+    class InputHandler {
+        constructer() {
+            this.keys = [];
+            window.addEventListener('keydown', function () {
+                console.log(e);
+            });
+        }
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+        
+    }
+
+    function displayStatusText() {
+        
+    }
+
+    const input = new InputHandler(); 
+
+    function animate() {
+        
+    }
 });
